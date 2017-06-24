c026fd79d9207a666283b8a94ab16cbe34ce5397
Kenny Dang <kdang1337@gmail.com>
2017-06-24 10:42:21 -0600

    Initial add of Mass Effect M35 Mako project

---
+++ MassEffectM35Mako/MassEffectM35Mako/MassEffectM35Mako/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  MassEffectM35Mako
+//
+//  Created by Kenny Dang on 6/24/17.
+//  Copyright © 2017 frooctus. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
